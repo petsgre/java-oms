4c89fdb90946e7c66ffd2f81d70773621870cd6f
张啸 <petsgre@gmail.com>
2020-10-15 16:24:38 +0800

    feat: add mybatis and hot load

--- src/main/java/com/site/site/SiteApplication.java
+++ src/main/java/com/site/site/SiteApplication.java
@@ -1,12 +1,19 @@
 package com.site.site;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan(basePackages = "com.site.site.dao")
 public class SiteApplication {
 
     public static void main(String[] args) {
+        int low = 3;
+        int height = 8;
+        int mid = (low + height) /2;
+        System.out.print(mid);
+        System.out.print(000000000000);
         SpringApplication.run(SiteApplication.class, args);
     }
 
--- src/main/java/com/site/site/controller/HelloWorldController.java
+++ src/main/java/com/site/site/controller/HelloWorldController.java
@@ -1,5 +1,7 @@
 package com.site.site.controller;
 
+import com.site.site.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,12 +11,39 @@ import org.springframework.web.bind.annotation.RestController;
  */
 @RestController
 public class HelloWorldController {
+    @Autowired //自动连接到UserService Bean
+    private UserService userService;
+
     @RequestMapping("/hello")
     public String sd() {
-        return "Hello World";
+        int low = 3;
+        int height = 10;
+        int mid = (low + height) /2;
+        System.out.println(mid);
+        System.out.println("111asdasd");
+	    System.out.println("asdasd");
+	    System.out.println(123);
+        return userService.show();
+//        return "Hello World";
     }
+
+    @RequestMapping(value = "/showdao")
+    public Object showDao(int age) {
+        return userService.showDao(age);
+    }
+
+    @RequestMapping(value = "/showall")
+    public Object showAll() {
+        return userService.showAll();
+    }
+
+    @RequestMapping(value = "/insertdao")
+    public Object insertUser(int age,String name){
+        return userService.insert(name,age);
+    }
+
     @RequestMapping(value = { "/" }, method = { RequestMethod.GET })
-    public String ndex() {
+    public String index() {
         return "forward:/index.html";
     }
 
